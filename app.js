e78147984fcc63fb30df01aad1629d869634b1ce
Jalol <jalolkurbanov11@gmail.com>
2023-09-30 20:07:19 +0500

    s

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 let amount = +prompt('Введите количество');
 
 let users= {};
-
+for(let q = 1;q<=amount;q++){
+    users[q]={};
+}
 for(let i = 1;i<=amount;i++){
     let userName = prompt("Как вас зовут?");
     let userAge = +prompt("Сколько вам лет?");
-    users[`name${i}`]=userName;
-    users[`age${i}`]=userAge;
+    users[i][`name`]=userName;
+    users[i][`age`]=userAge;
 }
 for(let k = 1;k<=amount;k++){
     console.log(`Пользователь - ${k}`);
-    console.log(`Имя - ${users[`name`+k]}`);
-    console.log(`Возраст - ${users[`age`+k]}`);
+    console.log(`Имя - ${users[k][`name`]}`);
+    console.log(`Возраст - ${users[k][`age`]}`);
 }
+console.log(users);
